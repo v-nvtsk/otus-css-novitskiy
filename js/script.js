93808a71bce102e308bd63b5fd2590adb23064a0
Novitskiy Vladislav <v@nvtsk.ru>
2023-09-07 18:28:47 +0300

    Add Scroll spy funtion

--- js/script.js
+++ js/script.js
@@ -1,39 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const sections = document.querySelectorAll('section[id]')
-  const sectionsY = {}
-  for (let i = 0; i < sections.length; i++) {
-    sectionsY[sections[i].id] = {}
-    sectionsY[sections[i].id].top = sections[i].offsetTop;
-    sectionsY[sections[i].id].height = sections[i].offsetHeight;
-  }
+  const sections = document.querySelectorAll('section[id]');
+  const anchors = document.querySelectorAll('a[href^="#"]');
+  anchors.forEach(el => {
+    el.addEventListener('click', (ev) => {
+      removeClassFromElements('.navigation-link.active');
 
-  document.addEventListener('scroll', function () {
-    let visibleSelector = '';
-    for (let el in sectionsY) {
-      if ((sectionsY[el].top - sectionsY[el].height < window.scrollY)) {
-        visibleSelector = el;
-      }
+      ev.target.classList.add('active')
+      document.body.dataset.active = ev.target.hash.slice(1);
+      document.body.classList.add('scrolling')
+    })
+  })
+
+  let scrollTimer = null;
+
+  document.addEventListener('scroll', function (ev) {
+    if (scrollTimer !== null) {
+      clearTimeout(scrollTimer)
     }
-    if (visibleSelector && (this.body.dataset.active !== visibleSelector)) {
-      if (document.body.dataset?.active) {
-        document.querySelector(`a[href="#${document.body.dataset.active}"]`)?.classList.remove('active')
 
-        const inactiveLinks = document.querySelectorAll(`a[href="#${document.body.dataset.active}"]`)
-        if (inactiveLinks) {
-          for (let i = 0; i < inactiveLinks.length; i++) {
-            inactiveLinks[i].classList.remove('active')
+    scrollTimer = setTimeout(() => {
+      document.body.classList.remove('scrolling')
+
+      let visibleSelector = null;
+      sections.forEach(el => {
+        const isElementHalfVisible = el.offsetTop < window.scrollY + window.innerHeight * 0.5
+        if (isElementHalfVisible) {
+          visibleSelector = el.id
+        };
+      })
+
+      isVisibleSelectorChanged = document.body.dataset.active !== visibleSelector
+      if (visibleSelector && isVisibleSelectorChanged) {
+        if (document.body.dataset?.active) {
+          removeClassFromElements('.navigation-link.active', 'active');
+        }
+        document.body.dataset.active = visibleSelector;
+        const activeLinks = document.querySelectorAll(`.navigation-link[href="#${visibleSelector}"]`)
+        if (activeLinks) {
+          for (let i = 0; i < activeLinks.length; i++) {
+            activeLinks[i].classList.add('active')
           }
         }
       }
-      document.body.dataset.active = visibleSelector;
-      const activeLinks = document.querySelectorAll(`a[href="#${visibleSelector}"]`)
-      if (activeLinks) {
-        for (let i = 0; i < activeLinks.length; i++) {
-          activeLinks[i].classList.add('active')
-        }
-      }
-    }
+    }, 100)
+
   })
 })
 
