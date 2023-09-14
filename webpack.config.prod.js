beadb7510e279a61f033c3479e55c7499aa160c0
Novitskiy Vladislav <v@nvtsk.ru>
2023-09-14 16:52:08 +0300

    Add webpack image minimizer config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+// const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -9,10 +12,6 @@ module.exports = {
     path: path.resolve(__dirname, 'prod'),
     filename: 'main-[hash].js',
     assetModuleFilename: '[name]-[hash][ext]'
-  },
-  devServer: {
-    hot: true,
-    port: 3000
   },
   module: {
     rules: [
@@ -46,5 +45,43 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin({
     template: path.resolve(__dirname, './src/index.html'),
     filename: 'index.html'
-  })]
+  })],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new ImageMinimizerPlugin(
+        {
+          minimizer: {
+            implementation: ImageMinimizerPlugin.sharpMinify,
+            options: {
+              encodeOptions: {
+                toFormat: { format: 'webp' },
+                jpeg: {
+                  // https://sharp.pixelplumbing.com/api-output#jpeg
+                  quality: 90,
+                },
+                webp: {
+                  // https://sharp.pixelplumbing.com/api-output#webp
+                  lossless: true,
+                },
+                avif: {
+                  // https://sharp.pixelplumbing.com/api-output#avif
+                  lossless: true,
+                },
+
+                // png by default sets the quality to 100%, which is same as lossless
+                // https://sharp.pixelplumbing.com/api-output#png
+                png: {},
+
+                // gif does not support lossless compression at all
+                // https://sharp.pixelplumbing.com/api-output#gif
+                gif: {},
+              },
+            },
+          }
+        }
+      ),
+      // new ImageminWebpWebpackPlugin(),
+    ],
+  },
 }
