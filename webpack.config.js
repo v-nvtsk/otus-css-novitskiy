8a50ddca626ee2dd4b543462304e043351e5aa0d
Novitskiy Vladislav <v@nvtsk.ru>
2023-09-23 23:56:37 +0300

    Remove filename generators for assets

--- webpack.config.js
+++ webpack.config.js
@@ -25,16 +25,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: '[name]-[hash][ext]'
-        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: '[name]-[hash][ext]'
-        }
       },
       {
         test: /\.html$/i,
