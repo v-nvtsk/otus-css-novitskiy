41251affa65d557be97669cbbef46a50d77f063b
Novitskiy Vladislav <v@nvtsk.ru>
2023-09-29 23:54:55 +0300

    Add webpack prod.config make js,css inline in html

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -51,7 +51,8 @@ module.exports = {
   },
   plugins: [new HtmlWebpackPlugin({
     template: path.resolve(__dirname, './src/index.html'),
-    filename: 'index.html'
+    filename: 'index.html',
+    inlineSource: '.(js|css)$' // embed all javascript and css inline
   })],
   optimization: {
     minimize: true,
